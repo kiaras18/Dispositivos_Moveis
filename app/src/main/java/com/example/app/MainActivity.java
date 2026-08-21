eb7b4fa138c9d935d98ca4ba959a23dbeee55bc9
oliver-santos <olivercs1108@gmail.com>
2026-08-21 12:09:42 -0300

    primeiro commit

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.app;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,25 +11,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    int contador;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Button b = findViewById(R.id.button);
-        TextView tv = findViewById(R.id.textView);
-
-        b.setOnClickListener(view -> {
-            contador++;
-            tv.setText(Integer.toString(contador));
-        });
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button b = findViewById(R.id.button);
+        TextView tv = findViewById(R.id.textView);
 
+        EditText edmin = findViewById(R.id.edmin);
+        EditText edmax = findViewById(R.id.edmax);
+
+        tv.setText(0);
+
+        b.setOnClickListener(view -> {
+            int min = Integer.parseInt(edmin.getText().toString());
+            int max = Integer.parseInt(edmax.getText().toString());
+            Random random = new Random();
+            int valor =random.nextInt();
+            tv.setText(Integer.toString(valor));
+        });
     }
 }
